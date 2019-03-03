7aaeccc88921ba90df09d39cb2dcbdd3088825ab
Jack Bransfield <bransfieldjack@gmail.com>
2019-03-03 10:08:15 +0000

    mesage

--- innablr_technical_challenge/settings.py
+++ innablr_technical_challenge/settings.py
@@ -88,11 +88,9 @@ WSGI_APPLICATION = 'innablr_technical_challenge.wsgi.application'
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 # }
-
 DATABASES  = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
+            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
